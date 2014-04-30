3fdf427b6e8a351a1330fc39aad1f7af95e08f62
Trey Duskin <trey@wilcoweb.net>
2014-04-30 07:41:01 -0400

    Licenses, updated author email, corrected description
    Added apache license to all source files
    Updated author email address
    Corrected description of S3Library docstring

--- src/S3Library/__init__.py
+++ src/S3Library/__init__.py
@@ -1,11 +1,31 @@
+'''
+Created on Mar 6, 2013
+
+@author: Trey Duskin <trey@wilcoweb.net>
+
+Copyright 2013 Maldivica (www.maldivica.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
 from keywords import Keywords
 
 __version__ = '0.01'
 
 class S3Library(Keywords):
     """
-    CloudScalerLibrary provides Robot Framework keywords for interacting with 
-    a Caringo CAStor Object Store via a CloudScaler Gateway
+    S3Library provides Robot Framework keywords for interacting with 
+    Amazon Web Services Simple Storage Service (S3) object storage 
     """
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     ROBOT_LIBRARY_VERSION = __version__
--- src/S3Library/keywords.py
+++ src/S3Library/keywords.py
@@ -1,7 +1,21 @@
 '''
 Created on Mar 6, 2013
 
-@author: Trey Duskin <trey@maldivica.com>
+@author: Trey Duskin <trey@wilcoweb.net>
+
+Copyright 2013 Maldivica (www.maldivica.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 '''
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 from boto.s3.key import Key
